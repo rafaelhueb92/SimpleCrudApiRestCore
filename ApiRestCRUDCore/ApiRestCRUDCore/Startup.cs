6c24e3ff9be973a27c12f18e583a56738977a56a
Rafael Hueb <rafaelhueb@hotmail.com>
2018-12-08 18:22:37 -0200

    Inclusão do Swagger

--- ApiRestCRUDCore/ApiRestCRUDCore/Startup.cs
+++ ApiRestCRUDCore/ApiRestCRUDCore/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ApiRestCRUDCore
 {
@@ -45,11 +46,39 @@ namespace ApiRestCRUDCore
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Api Core Restfull CRUD",
+                    Description = "A simple example ASP.NET Core Web API Rest With Dapper And EF, Created By Rafael Hueb",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Rafael Hueb",
+                        Email = "rafaelhueb92@gmail.com",
+                        Url = "https://www.instagram.com/ragahu92/"
+                    }
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api Core Restfull CRUD");
+            });
 
             if (env.IsDevelopment())
             {
